152e8c73e7dba36dcf8bb1a0061a7c0cd1ea46db
Damien George <dpgeorge@georgerobotics.com.au>
2023-03-05 10:05:28 +1100

    tools: Add code formatter tool and script for running CI commands.
    Base on https://github.com/micropython/micropython files.
    
    Signed-off-by: Damien George <dpgeorge@georgerobotics.com.au>

---
+++ tools/ci.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+if which nproc > /dev/null; then
+    MAKEOPTS="-j$(nproc)"
+else
+    MAKEOPTS="-j$(sysctl -n hw.ncpu)"
+fi
+
+########################################################################################
+# code formatting
+
+function ci_code_formatting_setup {
+    sudo apt-get install uncrustify
+    pip3 install black
+    uncrustify --version
+    black --version
+}
+
+function ci_code_formatting_run {
+    tools/codeformat.py -v
+}
+
+########################################################################################
+# code spelling
+
+function ci_code_spell_setup {
+    pip install codespell
+}
+
+function ci_code_spell_run {
+    codespell firmware/ src/
+}
