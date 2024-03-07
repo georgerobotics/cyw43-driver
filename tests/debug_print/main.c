9c3cb4b47e05b1fe02ad15d0fd7ba7526913e151
Damien George <dpgeorge@georgerobotics.com.au>
2024-03-07 12:20:00 +1100

    tests: Remove obsolete storage_read_blocks() dummy functions.
    They were made obsolete in b670b9b6bedd095147ac2c9e1b827b3fcce63f51.
    
    Signed-off-by: Damien George <dpgeorge@georgerobotics.com.au>

--- tests/debug_print/main.c
+++ tests/debug_print/main.c
@@ -22,10 +22,6 @@ void cyw43_cb_tcpip_set_link_down(cyw43_t *self, int itf) {
 void cyw43_cb_process_ethernet(void *cb_data, int itf, size_t len, const uint8_t *buf) {
 }
 
-uint32_t storage_read_blocks(uint8_t *dest, uint32_t block_num, uint32_t num_blocks) {
-    return 0;
-}
-
 int main(int argc, char **argv) {
     cyw43_init(&cyw43_state);
     cyw43_wifi_set_up(&cyw43_state, CYW43_ITF_STA, true, CYW43_COUNTRY_WORLDWIDE);
