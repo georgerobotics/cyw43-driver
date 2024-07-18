4649aa91fe409d5ef6af46921fd92d85352ef541
iabdalkader <i.abdalkader@gmail.com>
2024-07-18 14:16:08 +0300

    src/cyw43_bt: Load the firmware defined in the config option.
    Signed-off-by: iabdalkader <i.abdalkader@gmail.com>

--- src/cyw43_bt.c
+++ src/cyw43_bt.c
@@ -34,7 +34,7 @@
 #if MICROPY_PY_NETWORK_CYW43
 
 #include "lib/cyw43-driver/src/cyw43_config.h"
-#include "lib/cyw43-driver/firmware/cyw43_btfw_4343A1.h"
+#include CYW43_BT_FIRMWARE_INCLUDE_FILE
 
 // Provided by the port, and also possibly shared with the stack.
 extern uint8_t mp_bluetooth_hci_cmd_buf[4 + 256];
@@ -214,7 +214,7 @@ int mp_bluetooth_hci_controller_init(void) {
     mp_bluetooth_hci_uart_set_baudrate(MICROPY_HW_BLE_UART_BAUDRATE_DOWNLOAD_FIRMWARE);
     #endif
 
-    cywbt_download_firmware((const uint8_t *)&cyw43_btfw_4343A1[0]);
+    cywbt_download_firmware((const uint8_t *)bt_fw_data);
 
     // Reset
     cywbt_hci_cmd(0x03, 0x0003, 0, NULL);
